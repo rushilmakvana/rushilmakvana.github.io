1f1cdeaed8506a3321bf8058775028679adc1929
rushilmakvana <rushilmakvana@gmail.com>
2021-05-12 11:57:45 +0530

    update

--- src/Main.jsx
+++ src/Main.jsx
@@ -72,6 +72,11 @@ const Main = () => {
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
         crossorigin="anonymous"
       />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
+        rel="stylesheet"
+      ></link>
       <Header />
       <div className="left-pane">
         <div className="small">
